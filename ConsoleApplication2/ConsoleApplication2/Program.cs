c5a930c8a952dfb01fbb8376b18440e4aecee3ae
rovsk1y <156992607+rovsk1y@users.noreply.github.com>
2024-01-18 15:15:49 +0800

    Add files via upload

---
+++ ConsoleApplication2/ConsoleApplication2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine ("hello world KOKOS");
+            Console.ReadLine(); // ИС1-201 Кирилл Завьялов
+        }
+    }
+}
