0c81fbaf01e3b042fc51d1d2bdd12a532da3600f
rovsk1y <156992607+rovsk1y@users.noreply.github.com>
2024-01-18 15:16:47 +0800

    Add files via upload

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -11,7 +11,7 @@ namespace ConsoleApplication1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.ReadLine();
+            Console.ReadLine(); // ИС1-201 Уваровский Александр Дмитриевич
         }
     }
 
